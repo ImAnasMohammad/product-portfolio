2f479e1a105aa66ac6d43e8d8262f38960e7aada
ImAnasMohammad <webdevelop143@gmail.com>
2024-07-09 20:55:47 +0530

    changed main section  and features

--- main.js
+++ main.js
@@ -11,30 +11,8 @@ const variationSliderProgressBar = document.querySelector('#slider-progress');
 
 // default for Dark Mode
 
-const themeBGColor ="#0A0A0A" ;
-const themeColor = "#fff6e9";
-const liteBGColor = "rgb(0 0 0)";
-const liteColor = "#c8c8c8";
 const changeRootVariableValue = (variable,value)=>{
       root.style.setProperty(variable, value);
-}
-const changeThemeMode = ()=>{
-      if(isDarkMode){
-            themeToggleBtn.classList.remove('day')
-            themeToggleBtn.classList.add('night')
-            setThemeColor(themeColor,themeBGColor,liteColor,liteBGColor)
-      }else{
-            themeToggleBtn.classList.remove('night')
-            themeToggleBtn.classList.add('day')
-            setThemeColor(themeBGColor,themeColor,liteBGColor,liteColor)
-      }
-}
-
-const setThemeColor = (color,bgColor,liteColor,liteBGColor)=>{
-      changeRootVariableValue('--themeColor', color);
-      changeRootVariableValue('--themeBGColor',bgColor);
-      changeRootVariableValue('--lite-theme-color', liteColor);
-      changeRootVariableValue('--lite-theme-BGColor', liteBGColor);
 }
 
 
@@ -42,26 +20,6 @@ const setThemeColor = (color,bgColor,liteColor,liteBGColor)=>{
       let loaderWrapper = document.querySelector('.loader-wrapper');
       document.body.classList.remove('loaded')
       loaderWrapper.classList.add('active');
-      // Function to detect the current theme
-      function detectTheme() {
-            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                  isDarkMode = true;
-            } else {
-                  isDarkMode = false;
-            }
-            changeThemeMode()
-      }
-  
-      detectTheme()
-      // Listen for changes in the color scheme preference
-      window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-            if (event.matches) {
-                  isDarkMode=true;
-            } else {
-                  isDarkMode = false;
-            }
-            changeThemeMode()
-      });
 })()
 
 
@@ -90,10 +48,6 @@ window.onscroll = function () {
       }
 };
 
-themeToggleBtn.addEventListener('click',()=>{
-      changeThemeMode()
-      isDarkMode = !isDarkMode
-})
 
 
 const observer = new IntersectionObserver((entries)=>{
@@ -110,7 +64,6 @@ animateElements.forEach(el=>observer.observe(el))
 
 
 variationSliderProgressBar.addEventListener('input',(e)=>{
-      console.log(e.target.valueAsNumber)
       changeRootVariableValue('--before-width',e.target.valueAsNumber+"%");
 })
 
